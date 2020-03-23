65533467de92a701a643434798c6725d578010e9
Thales Lima <lthales53@gmail.com>
2020-03-23 20:32:49 -0300

    Implementing operation of dot

--- scripts/Controller/CalcController.js
+++ scripts/Controller/CalcController.js
@@ -28,6 +28,8 @@ class CalcController {
     }
     clearAll() {
         this._operation = [];
+        this._lastNumber = '';
+        this._lastOperator = '';
         this.setLastNumberToDisplay();
 
     }
@@ -117,10 +119,7 @@ class CalcController {
 
                 this.setLastOperation(value)
 
-            } else if (isNaN(value)) {
-                console.log("outra coisa" + value);
             } else {
-
                 this.pushOperation(value);
                 this.setLastNumberToDisplay();
             }
@@ -132,7 +131,7 @@ class CalcController {
 
             } else {
                 let newValue = this.getLastOperation().toString() + value.toString();
-                this.setLastOperation(parseInt(newValue));
+                this.setLastOperation((newValue));
 
                 this.setLastNumberToDisplay();
             }
@@ -140,6 +139,18 @@ class CalcController {
     }
     setError() {
         this.displayCalc = "Error";
+    }
+    addDot(){
+       let lastOperation =  this.getLastOperation();
+      
+       if(typeof lastOperation === 'string' && lastOperation.split('').indexOf('.') > -1) return;
+      
+       if(this.isOperator(lastOperation) || !lastOperation){
+           this.pushOperation('0.');
+       }else{
+           this.setLastOperation(lastOperation.toString() + '.');
+       }
+       this.setLastNumberToDisplay();
     }
     execBtn(value) {
 
@@ -169,7 +180,7 @@ class CalcController {
                 this.calc();
                 break;
             case 'ponto':
-                this.addOperation('.');
+                this.addDot('.');
                 break;
 
             case '0':

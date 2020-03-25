f332cf7b5d9ef843eb3fbf3c6358de23bffcb111
Thales Lima <lthales53@gmail.com>
2020-03-25 19:26:01 -0300

    Limiting digit numbers

--- scripts/Controller/CalcController.js
+++ scripts/Controller/CalcController.js
@@ -313,6 +313,10 @@ class CalcController {
     return this._displayCalcEl.innerHTML;
   }
   set displayCalc(value) {
+    if(value.toString().length > 10){
+      this.setError();
+      return false;
+    }
     this._displayCalcEl.innerHTML = value;
   }
   get currentDate() {
